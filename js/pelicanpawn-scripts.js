0ef26158a82b23832d68515a3befd90a379e43a8
childressdev <jcampbell@childressagency.com>
2017-09-20 17:25:57 -0400

    styled through cash loans

--- js/pelicanpawn-scripts.js
+++ js/pelicanpawn-scripts.js
@@ -1,3 +1,12 @@
 jQuery(document).ready(function($){
-
+  
+  if (typeof $.fn.swiperight == 'function') {
+    $('.carousel.slide').swiperight(function () {
+      $(this).carousel('prev');
+    });
+    $('.carousel.slide').swipeleft(function () {
+      $(this).carousel('next');
+    });
+  }
+ 
 });
