c3e1e4c70fdd8d98e55e7f398884cfcc6a7a467e
childressdev <jcampbell@childressagency.com>
2017-09-21 10:37:09 -0400

    home demo styled

--- js/pelicanpawn-scripts.js
+++ js/pelicanpawn-scripts.js
@@ -8,5 +8,31 @@ jQuery(document).ready(function($){
       $(this).carousel('next');
     });
   }
- 
+
+  if (typeof $.fn.lightSlider == 'function') {
+    $('#locations .location-row').lightSlider({
+      item: 3,
+      loop: true,
+      slideMargin: 0,
+      auto: true,
+      pauseOnHover: true,
+      pager: false,
+      keyPress: true,
+      pause: 3000,
+      responsive: [
+        {
+          breakpoint: 991,
+          settings: {
+            item: 2
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            item: 1
+          }
+        }
+      ]
+    });
+  }
 });
